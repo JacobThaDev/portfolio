9eb61e6465c78ad10e502ea5f574a9e4c75e18de
OG KingFox <rune.evo2012@gmail.com>
2023-10-18 03:21:07 -0500

    updated metadata

--- layouts/MetaData.tsx
+++ layouts/MetaData.tsx
@@ -10,8 +10,8 @@ export const MetaData = ({ title, desc, }: MetaProps) => {
 
 	return (
 		<NextHead>
-			<title>{`${title ? title+" | Foxtrot Labs" : "Foxtrot Labs"}`}</title>
-			<meta name="description" content={`${desc ? desc : "Get access to hundreds of beautiful website templates, components, and more with our online library. Get started today for free, or pick up our premium plan for exclusive all-access."}`} />
+			<title>{`${title ? title+" | Credo Construction" : "Credo Construction"}`}</title>
+			<meta name="description" content={`${desc ? desc : "Top general contractor in Whatcom and Skagit Counties in Washington, we offer a variety of building services tailored to meet your needs."}`} />
 			<meta name="viewport" key="viewport" content="width=device-width, initial-scale=0.87, shrink-to-fit=no" />
 			<link href="/favicon.svg" rel="icon" />
 		</NextHead>
