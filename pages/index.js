be6f066a8acd7779f6e11e574083a75d1d79ca76
OG KingFox <rune.evo2012@gmail.com>
2024-02-24 11:41:23 -0600

    updated production env

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,10 @@
 import GlobalLayout from '@/layouts/GlobalLayout';
-import Image from 'next/image';
-import Link from 'next/link';
 
 function Homepage() {
 
     return (
         <GlobalLayout title="Home">
-            
+
         </GlobalLayout>
     );
 
