7ecab70676a3da88c884b80751975d3b766eeb3e
OG KingFox <rune.evo2012@gmail.com>
2024-02-25 02:18:34 -0600

    fix build error

--- pages/index.js
+++ pages/index.js
@@ -95,7 +95,7 @@ function Homepage() {
                             </p>
 
                             <p className="text-lg mb-10">
-                                I'm <strong>Jacob Smith</strong>, a well respected and independent website developer and avid gamer from Texarkana, Texas. 
+                                I&apos;m <strong>Jacob Smith</strong>, a well respected and independent website developer and avid gamer from Texarkana, Texas. 
                                 Every website is hand crafted to be sleek and professional using NextJS, NodeJS, and TailwindCSS. I focus on clean and maintainable
                                 code and like to experiment in my free time. I also like to squash edge-cases for fun.
                             </p>
