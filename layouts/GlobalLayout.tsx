74c56cea2a7726f4e8359af2626e5a932d04e2de
JacobThaDev <rune.evo2012@gmail.com>
2024-03-08 18:12:30 -0600

    fixed doubled import

--- layouts/GlobalLayout.tsx
+++ layouts/GlobalLayout.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import { MetaData } from "./MetaData";
 import Navbar from "@/components/global/Navbar";
 import Footer from "@/components/global/Footer";
-import Footer from "@/components/global/Footer";
 
 interface LayoutProps {
     children: React.ReactNode;
